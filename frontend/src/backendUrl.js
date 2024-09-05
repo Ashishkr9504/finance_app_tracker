b9a3cc39c1a2033865ebad3b5a94fa14095b2349
Ashish Kumar <137616225+Ashishkr9504@users.noreply.github.com>
2024-09-05 20:44:09 +0530

    Update backendUrl.js

--- frontend/src/backendUrl.js
+++ frontend/src/backendUrl.js
@@ -1,4 +1,4 @@
- const url = "https://finance-app-tracker-ari.onrender.com/"
+ const url = "https://finance-app-tracker-ari.onrender.com"
  // const url = "http://localhost:5000"
 
 export default url
